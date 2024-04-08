974da48f315c4d01bbe496c7123a8ad1ead665f2
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-04-08 15:09:10 +0200

    [add] header, navBar, input search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,15 @@
 <script>
 import {store} from '../assets/data/store';
+import NavBar from './partials/NavBar.vue';
     export default {
+        components:{
+            NavBar
+        },
+        
         data(){
             return{
                 store
             }
-        },
-
-        computed(){
-            
         },
 
         mounted(){
@@ -18,8 +19,39 @@ import {store} from '../assets/data/store';
 </script>
 
 <template>
-    <header class="">
-        <h1>{{  }}</h1>
+    <header class="d-flex">
+        <div class="main-container container-fluid d-flex justify-content-between align-items-center px-5">
+            <div class="d-flex align-items-center">
+                <NavBar />
+            </div>
+            
+            <!-- logo -->
+            <div class="logo text-center">
+                <img src="/dark-logo.png" alt="">
+            </div>
+            
+            <div class="search-bar d-flex justify-content-end">
+                <!-- icone -->
+                <div class="icons me-2 ">
+                    <span>
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </span>
+                    <span>
+                        <i class="fa-regular fa-circle-user"></i>
+                    </span>
+                </div>
+                <!-- input search -->
+                <div class="input-search">
+                    <span>
+                        <input type="text" placeholder="Search...">
+                    </span>
+                    <span>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </span>
+                    
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -31,6 +63,44 @@ import {store} from '../assets/data/store';
         width: 100%;
         min-height: 80px;
         background: rgba(255, 0, 0, 0.562);
+        .logo img{
+            width: 60%;
+        }
+        .main-container .search-bar{
+            .icons{
+                width: 75px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 0 10px;
+                i{
+                    font-size: 1.1rem;
+                    cursor: pointer;
+                    &:hover{
+                        color: grey;
+                    }
+                }
+            }
+        }
+        .main-container > div{
+            width: calc(100% / 3);
+            .input-search{
+                background-color: rgba(128, 128, 128, 0.589);
+                border-radius: 5px;
+                padding: 7px 10px;
+                input[type="text"]{
+                    border: none;
+                    background-color: transparent;
+                    &:focus{
+                        border: none;
+                        box-shadow: none;
+                    }
+                }
+                span:last-child{
+                    padding: 10px;
+                }
+            }
+        }
     }
 
 </style>
---
+++ src/components/partials/NavBar.vue
@@ -0,0 +1,44 @@
+<script>
+import {store} from '../../assets/data/store';
+    export default {
+        data(){
+            return{
+                store
+            }
+        },
+
+        computed:{
+            headerNavBar(){
+                return this.store.dBase.header[0]
+            }
+        },
+    }
+</script>
+
+<template>
+    <ul class="d-flex">
+        <li
+            v-for="(item, index) in headerNavBar"
+            :key="index"
+        ><a href="#">{{ item }}</a></li>
+        
+    </ul>
+</template>
+
+
+<style lang="scss" scoped>
+    ul{
+        padding-left: 0;
+        margin-bottom: 0;
+        li{
+            border: 1px solid black;
+            &:hover{
+                background-color: yellow;
+            }
+            a{
+                padding: 5px 10px;
+                
+            }
+        }
+    }
+</style>
