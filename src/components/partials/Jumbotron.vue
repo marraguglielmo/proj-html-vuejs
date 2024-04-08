cb0f832e47fb3a7f10cba5f6ed573f55f671874c
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-04-08 19:42:20 +0200

    [add] aside bar

--- src/components/partials/Jumbotron.vue
+++ src/components/partials/Jumbotron.vue
@@ -13,18 +13,26 @@ import {store} from '../../assets/data/store';
             }
         },
         mounted(){
-            console.log(this.jumbo);
+
         }
     }
 </script>
 
 <template>
-
+    
     <section class="jumbotron d-flex" scoped>
+        <!-- barra laterale -->
+        <div class="aside-bar d-flex flex-column justify-content-between align-items-center position-sticky">
+                <i class="fa-solid fa-cart-shopping"></i>
+                <i class="fa-solid fa-book"></i>
+                <i class="fa-solid fa-life-ring"></i>
+                <i class="fa-solid fa-border-top-left"></i>
+            </div>  
         <div class="container d-flex flex-column justify-content-center text-center text-white">
             <h3 class="fw-bold">Commence Business</h3>
             <h1 class="mb-4">Distant Mentoring Program</h1>
             <button class="btn btn-white bg-white fw-bold">Download free guidebook</button>
+            
         </div>
     </section>
     
@@ -45,6 +53,25 @@ import {store} from '../../assets/data/store';
         color: $darkGreen;
         width: 300px;
         margin: 0 auto;
+    }
+    .aside-bar{
+        background-color: $white;
+        width: 50px;
+        height: 150px;
+        color: $black;
+        top: 150px;
+        left: 100vw;
+        padding: 10px 13px;
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+        font-size: 1.1rem;
+        i{
+            cursor: pointer;
+            &:hover{
+                scale: 1.2;
+                transition: scale .2s;
+            }
+        }
     }
 }
 
